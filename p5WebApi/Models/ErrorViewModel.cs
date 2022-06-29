d50832f40d2389cf179bfd19ae5e955504cddd27
Luan <krytanx@gmail.com>
2022-06-29 07:59:01 +0200

    Deleted api

--- p5WebApi/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace p5WebApi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
