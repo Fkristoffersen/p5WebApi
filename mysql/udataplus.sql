f0a79ae9a046cabe96bb0e34be7cca4e5d246715
Luan <42214331+Krytan@users.noreply.github.com>
2022-07-01 08:24:58 +0200

    Mysql select stored procedures

--- mysql/udataplus.sql
+++ mysql/udataplus.sql
@@ -4,22 +4,22 @@ CREATE DATABASE udataplus;
 USE udataplus;
 
 CREATE TABLE IF NOT EXISTS student (
-	idStudent INT NOT NULL AUTO_INCREMENT,
+	StudentId INT NOT NULL AUTO_INCREMENT,
 	FirstName VARCHAR(45) NOT NULL,
 	LastName VARCHAR(45) NOT NULL,
 	Address VARCHAR(45) NOT NULL,
 	Postal VARCHAR(45) NOT NULL,
 	Phone VARCHAR(45) NOT NULL,
-  PRIMARY KEY (idStudent));
+  PRIMARY KEY (StudentId));
   
   CREATE TABLE IF NOT EXISTS teacher (
-	idteacher INT NOT NULL AUTO_INCREMENT,
+	TeacherId INT NOT NULL AUTO_INCREMENT,
 	FirstName VARCHAR(45) NOT NULL,
 	LastName VARCHAR(45) NOT NULL,
 	Address VARCHAR(45) NOT NULL,
 	Postal VARCHAR(45) NOT NULL,
 	Phone VARCHAR(45) NOT NULL,
-  PRIMARY KEY (idteacher));
+  PRIMARY KEY (TeacherId));
 
 
 INSERT INTO student (FirstName,LastName,Address,Postal,Phone)
@@ -30,7 +30,17 @@ VALUES ('Amanda','Nicolinelsen','305 - 14th Ave. S. Suite 3B', '2700', '81946482
         ('Enrik','Nicolinelsen','305 - 14th Ave. S. Suite 3B', '2700', '81946482'),
         ('Bingus','Nicolinelsen','305 - 14th Ave. S. Suite 3B', '2700', '81946482'),
         ('Luan','Nicolinelsen','305 - 14th Ave. S. Suite 3B', '2700', '81946482'),
-        ('Dona','Nicolinelsen','305 - 14th Ave. S. Suite 3B', '2700', '81946482')
+        ('Dona','Nicolinelsen','305 - 14th Ave. S. Suite 3B', '2700', '81946482');
+
+INSERT INTO teacher (FirstName,LastName,Address,Postal,Phone)
+VALUES ('Bo','Nicolinelsen','305 - 14th Ave. S. Suite 3B', '2700', '81946482'),
+		('John','Nicolinelsen','305 - 14th Ave. S. Suite 3B', '2700', '81946482'),
+        ('Pelle','Nicolinelsen','305 - 14th Ave. S. Suite 3B', '2700', '81946482'),
+        ('Jack','Nicolinelsen','305 - 14th Ave. S. Suite 3B', '2700', '81946482'),
+        ('Nicolaj','Nicolinelsen','305 - 14th Ave. S. Suite 3B', '2700', '81946482'),
+        ('Ricardo','Nicolinelsen','305 - 14th Ave. S. Suite 3B', '2700', '81946482'),
+        ('Steve','Nicolinelsen','305 - 14th Ave. S. Suite 3B', '2700', '81946482'),
+        ('Rekt','Nicolinelsen','305 - 14th Ave. S. Suite 3B', '2700', '81946482');
 
 
 DELIMITER $$
@@ -39,10 +49,21 @@ CREATE PROCEDURE FindAll(
 )
 BEGIN
 
-    SELECT FirstName, LastName
-    FROM student, teacher
-    ORDER BY student;
+    (SELECT FirstName, LastName from student)
+    UNION 
+    (SELECT FirstName, LastName FROM teacher)
+    order by FirstName;
 
+
+END$$
+
+CREATE PROCEDURE FindTeachers(
+	IN teacher_name VARCHAR(20)
+)
+BEGIN 
+    SELECT *
+    FROM teacher 
+    WHERE FirstName = teacher_name;
 
 END$$
 
@@ -64,11 +85,12 @@ BEGIN
 
     DELETE
     FROM student
-    WHERE idStudent = student_id;
+    WHERE StudentId = student_id;
 
 END$$
 DELIMITER ; 
 
-CALL FindStudents('Frederik');
+CALL FindStudents('Luan');
 CALL DeleteStudents(1);
-CALL FindAll('dogs')
+CALL FindAll('dogs');
+CALL FindTeachers()
